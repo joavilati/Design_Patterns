3dafe35ff0001e59f630c9f14078ce291ed02011
Jhonata <jhonata.avila@taglivros.com.br>
2020-08-23 12:30:12 -0300

    feat(activity_aluguel_carros.xml): terminar layout de themplate method.

---
+++ app/src/main/java/com/example/design_patterns/template_method/AluguelCarrosActivity.java
@@ -0,0 +1,16 @@
+package com.example.design_patterns.template_method;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.design_patterns.R;
+
+public class AluguelCarrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluguel_carros);
+    }
+}
